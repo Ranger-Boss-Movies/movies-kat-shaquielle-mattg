d51809c3d2e3245272811562ad7aae913268139e
Matthew Grayson <matthew.alan.grayson@gmail.com>
2022-07-26 14:50:44 -0500

    added modal functionality and partial autofill

--- js/views/Home.js
+++ js/views/Home.js
@@ -31,11 +31,11 @@ function getTopMovieHTML() {
                     <div class="modal-body">
                         <form>
                             <div class="mb-3">
-                                <label for="movie-title" class="col-form-label">Recipient:</label>
+                                <label for="movie-title" class="col-form-label">Title:</label>
                                 <input type="text" class="form-control" id="movie-title">
                             </div>
                             <div class="mb-3">
-                                <label for="message-text" class="col-form-label">Message:</label>
+                                <label for="message-text" class="col-form-label">Rating:</label>
                                 <textarea class="form-control" id="message-text"></textarea>
                             </div>
                         </form>
@@ -61,9 +61,9 @@ function displayMovieHTML(movies) {
     for (let i = 0; i < movies.length; i++) {
         html +=
             `
-                <div class="col-sm-4 movieCol">
+                <div class="col-sm-4 movieCol" data-bs-toggle="modal" data-bs-target="#add-modal" data-bs-title="${movies[i].title}" data-bs-rating="${movies[i].rating}">
                       <p>${movies[i].title}</p>
-                      <p>Rating</p>
+                      <p>${movies[i].rating}</p>
                 </div>
     `;
     }
@@ -78,18 +78,31 @@ function getBottomMovieHTML() {
 }
 function addMovieEventListener() {
     const movieCol = document.querySelectorAll(".movieCol");
-    movieCol.addEventListener("click", function(event) {
+    movieCol.forEach(function(col) {
+        col.addEventListener('click', function(event) {
+            const addModal = document.getElementById('add-modal')
+            addModal.addEventListener('show.bs.modal', function(event) {
+                // Button that triggered the modal
+                let button = event.relatedTarget
+                // Extract info from data-bs-* attributes
+                let title = button.getAttribute('data-bs-title')
+                let rating = button.getAttribute('data-bs-rating')
 
+                // If necessary, you could initiate an AJAX request here
+                // and then do the updating in a callback.
+                //
+                // Update the modal's content.
+                const modalTitle = addModal.querySelector('.modal-title')
+                const modalBodyInput = addModal.querySelector('.modal-body input')
+
+                modalTitle.textContent = 'New message to ' + title
+                modalBodyInput.value = title
+            })
+        })
     })
 }
 
 export function HomeEvents() {
+    addMovieEventListener()
     // TODO: use an enum for message type
-    // const authority = getUserRole();
-    // const user = getUser();
-    // if(!user) {
-    //     showNotification("Welcome visitor", "secondary");
-    // } else {
-    //     showNotification("Welcome " + user.userName, "info");
-    // }
 }
