5c1159e8cf9137fdda5dd3f5356a0868e17396fb
Matthew Grayson <matthew.alan.grayson@gmail.com>
2022-07-26 14:13:59 -0500

    added modal html

--- js/views/Home.js
+++ js/views/Home.js
@@ -17,10 +17,36 @@ function getTopMovieHTML() {
                 <div class="inner-content text-center text-white p-5">
                     <h1 class="text uppercase">Welcome to Popcorn Cinema!</h1>
                     <p style="color: white">Best movies in the world!</p>
-                   
                 </div>
             </div>
         </header>
+
+        <div class="modal fade" id="add-modal" tabindex="-1" aria-labelledby="modalLabel" aria-hidden="true">
+            <div class="modal-dialog">
+                <div class="modal-content">
+                    <div class="modal-header">
+                        <h5 class="modal-title" id="modalLabel">New message</h5>
+                        <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+                    </div>
+                    <div class="modal-body">
+                        <form>
+                            <div class="mb-3">
+                                <label for="movie-title" class="col-form-label">Recipient:</label>
+                                <input type="text" class="form-control" id="movie-title">
+                            </div>
+                            <div class="mb-3">
+                                <label for="message-text" class="col-form-label">Message:</label>
+                                <textarea class="form-control" id="message-text"></textarea>
+                            </div>
+                        </form>
+                    </div>
+                    <div class="modal-footer">
+                        <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>
+                        <button type="button" class="btn btn-primary">Send message</button>
+                    </div>
+                </div>
+            </div>
+        </div>
         
         <main>
         <div class="container">
@@ -34,8 +60,8 @@ function displayMovieHTML(movies) {
     let html = "";
     for (let i = 0; i < movies.length; i++) {
         html +=
-        `
-                <div class="col-sm-4">
+            `
+                <div class="col-sm-4 movieCol">
                       <p>${movies[i].title}</p>
                       <p>Rating</p>
                 </div>
@@ -49,6 +75,12 @@ function getBottomMovieHTML() {
         </div>
             </div>
         </main>`;
+}
+function addMovieEventListener() {
+    const movieCol = document.querySelectorAll(".movieCol");
+    movieCol.addEventListener("click", function(event) {
+
+    })
 }
 
 export function HomeEvents() {
